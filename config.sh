741d519043ee18e280d2b00c2a70bfec287b48aa
Yossi Mesika <mesika@il.ibm.com>
2018-08-06 15:29:29 +0300

    Guestbook code pattern

---
+++ config.sh
@@ -0,0 +1,9 @@
+# Cluster A Kubeconfig context
+CLUSTER_A="cluster-IKS-context"
+
+# Cluster B Kubeconfig context
+CLUSTER_B="cluster-ICP-context"
+
+# Watson Tone Analyzer service credentials
+TONE_ANALYZER_USERNAME="username"
+TONE_ANALYZER_PASSWORD="password"
